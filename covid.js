2ca0985151cfd4cdfefffbc32f1b251227fe27b3
Sujit <sujeetnangare@gmail.com>
2024-08-25 15:45:09 +0530

    code refactor

--- covid.js
+++ covid.js
@@ -1,158 +1,68 @@
 var dt = new Date();
 document.getElementById("datetime").innerHTML = dt.toLocaleString();
 
-function getWorldData(){
-    fetch('https://api.covid19api.com/summary')
-    .then(response => response.json())
-    .then(data =>{
-
-        let worldData = `
-        
-        <td>${data.Global.TotalConfirmed}</td>
-        <td>${data.Global.TotalDeaths}</td>
-        <td>${data.Global.NewConfirmed}</td>
-       
-        
-        `
-        document.querySelector('#worldData').innerHTML = worldData;
+// Fetch global COVID-19 data
+function getWorldData() {
+  fetch("https://disease.sh/v3/covid-19/all")
+    .then((response) => response.json())
+    .then((data) => {
+      let worldData = `
+            <td>${data.cases}</td>
+            <td>${data.deaths}</td>
+            <td>${data.todayCases}</td>
+        `;
+      document.querySelector("#worldData").innerHTML = worldData;
     })
+    .catch((error) => console.error("Error fetching world data:", error));
 }
+
+// Fetch COVID-19 data for India
+function getIndiaData() {
+  fetch("https://disease.sh/v3/covid-19/countries/India")
+    .then((response) => response.json())
+    .then((data) => {
+      let indiaData = `
+            <td>${data.cases}</td>
+            <td>${data.deaths}</td>
+            <td>${data.todayCases}</td>
+        `;
+      document.querySelector("#indiaData").innerHTML = indiaData;
+    })
+    .catch((error) => console.error("Error fetching India data:", error));
+}
+
+// Fetch COVID-19 data for Maharashtra
+function getMaharashtraData() {
+  fetch("https://disease.sh/v3/covid-19/nyt/states/Maharashtra")
+    .then((response) => response.json())
+    .then((data) => {
+      let maharashtraData = `
+            <td>${data[0].cases}</td>
+            <td>${data[0].recovered}</td>
+            <td>${data[0].todayCases}</td>
+        `;
+      document.querySelector("#maharashtraData").innerHTML = maharashtraData;
+    })
+    .catch((error) => console.error("Error fetching Maharashtra data:", error));
+}
+
+// Fetch COVID-19 data for Pune
+function getPuneData() {
+  fetch("https://disease.sh/v3/covid-19/jhucsse/counties/Pune")
+    .then((response) => response.json())
+    .then((data) => {
+      let puneData = `
+            <td>${data[0].stats.confirmed}</td>
+            <td>${data[0].stats.recovered}</td>
+            <td>${data[0].stats.confirmed}</td>
+        `;
+      document.querySelector("#puneData").innerHTML = puneData;
+    })
+    .catch((error) => console.error("Error fetching Pune data:", error));
+}
+
+// Call functions to fetch and display the data
 getWorldData();
-
-
-function getIndiaData(){
-    fetch('https://api.covid19api.com/summary')
-    .then(response => response.json())
-    .then(data =>{
-
-        let indiaData = `
-        
-        <td>${data.Countries[76].TotalConfirmed}</td>
-        <td>${data.Countries[76].TotalDeaths}</td>
-        <td>${data.Countries[76].NewConfirmed}</td>
-       
-        
-        `
-        document.querySelector('#indiaData').innerHTML = indiaData;
-    })
-}
 getIndiaData();
-
-
-
-
-function getMaharashtraData(){
-    fetch('https://data.covid19india.org/v4/min/data.min.json')
-    .then(response => response.json())
-    .then(data =>{
-      
-        let maharashtraData = `
-        
-        <td>${data.MH.total.confirmed}</td>
-        <td>${data.MH.total.recovered}</td>
-        <td>${data.MH.delta21_14.confirmed}</td>
-        
-        `
-        document.querySelector('#maharashtraData').innerHTML = maharashtraData;
-    })
-}
 getMaharashtraData();
-
-
-
-function getPuneData(){
-    fetch('https://data.covid19india.org/v4/min/data.min.json')
-    .then(response => response.json())
-    .then(data =>{
-
-        let puneData = `
-        
-        <td>${data.MH.districts.Pune.total.confirmed}</td>
-        <td>${data.MH.districts.Pune.total.recovered}</td>
-        <td>${data.MH.districts.Pune.delta21_14.confirmed}</td>
-       
-        
-        `
-        document.querySelector('#puneData').innerHTML = puneData;
-    })
-}
 getPuneData();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // async function getCountryData(){
-    //     var url = "https://api.covid19api.com/countries";
-    //     $.get(url,function(individualCountryData){
-
-    //         for(let noofCountries=0;noofCountries<=individualCountryData.length-1;noofCountries++)
-    //         {
-
-    //             $("#selectCountry").append("<option value = " +individualCountryData[noofCountries].Country+ ">"+individualCountryData[noofCountries].Country+"</option>")
-    //         }
-    //     });
-    
-
-    // }   
-    // getCountryData();
-
-// const selectElem = document.querySelector('select#select');
-// function makenewoptionbox(data){
-//     const title = data.title;
-//     if(typeof title != "undefined"){
-//         const optionBox = document.createElement('option');
-//         optionBox.innerHTML = title;
-//         selectElem.appendChild(optionBox);
-//     }
-// }
-
-// function manipulationData(data){
-//     for (eachItem in data.countryitems[0]) {
-//         const singleData = data.countryItems[0][eachItem];
-
-//         makenewoptionbox(singleData);
-//     }
-// }
-
-// const fetchedData = fetch(
-//     "https://api.thevirustracker.com/free-api?countryTotals=ALL"
-// )
-//  .then((response) => {
-//     return response.json();
-//  })
-//  .then((data) => {
-//     manipulationData(data);
-//  });
